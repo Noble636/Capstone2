1c789e40b010a8435a39550066356f97dade6909
Noble636 <nikkoarangorin004@gmail.com>
2025-12-28 12:01:39 +0800

    Initial commit

--- src/components/Admin/AdminInbox.js
+++ src/components/Admin/AdminInbox.js
@@ -147,7 +147,7 @@ const AdminInbox = () => {
           sender_name: 'Admin',
           sender_type: 'admin',
           message: chatInput.trim(),
-          recipient_name: selectedConv.sender_name, // <-- THIS IS REQUIRED
+          recipient_name: selectedConv.sender_name,
         }),
       });
       if (res.ok) {
